a34c52f3a0a604b45bc0206ce0f1182bd06885f9
Shreyas Nair <126570047+Shreyas20004@users.noreply.github.com>
2025-07-05 15:04:52 +0000

    proper build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify'
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
