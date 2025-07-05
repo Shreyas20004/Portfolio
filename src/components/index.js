f1aef733d7999c0ca769c42ae5af18166f7cd183
Shreyas Nair <126570047+Shreyas20004@users.noreply.github.com>
2025-07-05 14:57:53 +0000

    fixes #1

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Contact from "./Contact";
 import Navbar from "./Navbar";
-import Projects from "./projects";
+import Projects from "./Projects";
 import Skills from "./Skills";
 import TimelineSection from "./Timeline";
 import Intro from "./Intro";
